dea264a2668f051c8328e646625c271861fa760b
sdenton <samdenton1457@gmail.com>
2021-05-01 19:19:22 -0400

    width fix

--- src/components/comingsoon.js
+++ src/components/comingsoon.js
@@ -1,21 +1,25 @@
 import React from 'react'
 import './comingSoon.css'
+import UseWindowDimensions from './windowWidth';
 
 export default function ComingSoon() {
+
+    const { height, width } = UseWindowDimensions();
+
     return (
         <div className="coming-soon">
-            
             <h2>
                 Coming Soon!
             </h2>
             <hr style={{margin:'3rem'}}></hr>
             <h4>
-            For Service and Information Now Please Contact:
+           
+                For Service and Information:
                 <br></br>
                 <br></br>
-                Email: Dentonslawnandtree@gmail.com
+                dentonslawnandtree@gmail.com
                 <br></br>
-                Phone: 519-709-5541
+                519-709-5541
             </h4>
         </div>
     )
--- src/components/logo.js
+++ src/components/logo.js
@@ -1,14 +1,22 @@
 import React from 'react'
 import './logo.css'
+import UseWindowDimensions from './windowWidth';
 
 export default function logo() {
+    const { height, width } = UseWindowDimensions();
+    let breakStr = '//';
+
+    if (width < 500) {
+        breakStr = <br></br>;
+    }
+    
     return (
         <div className="logo">
             <div style={{fontSize: '30px', fontWeight: '600'}}>
                 Denton's Lawn and Tree
             </div>
             <div style={{fontSize: '20px'}}>
-                Phone: (519)709-5541 // Email: dentonslawnandtree@gmail.com
+                dentonslawnandtree@gmail.com {breakStr} (519) 709-5541
             </div>
         </div>
     )
